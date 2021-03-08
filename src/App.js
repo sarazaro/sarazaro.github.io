aeda274dbcc3a65ff0d193e7564333f6ba7ac31d
Jessee Zhang <jesseelzhang@gmail.com>
2021-03-07 18:42:08 -0600

    Creates header and skeleton for app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { HashRouter, Route } from "react-router-dom";
+import ThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+import ButtonBase from "@material-ui/core/ButtonBase";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter basename="/">
+      <ThemeProvider theme={theme}>
+        <div className="app">
+          <div className="header-container">
+            <div className="header-logo">
+              <ButtonBase className="header-logo">Sara Zaro</ButtonBase>
+            </div>
+            <div className="header-right-items">
+              <div className="header-item-container">
+                <ButtonBase>
+                  <div className="header-item">Me in a nutshell</div>
+                </ButtonBase>
+              </div>
+              <div className="header-item-container">
+                <ButtonBase>
+                  <div className="header-item">My life</div>
+                </ButtonBase>
+              </div>
+              <div className="header-item-container">
+                  <ButtonBase>
+                    <div className="header-item">Social</div>
+                  </ButtonBase>
+              </div>
+              <div className="header-item-container">
+                  <ButtonBase>
+                    <div className="header-item">Resume</div>
+                  </ButtonBase>
+              </div>
+              <div className="header-item-container">
+                  <ButtonBase>
+                    <div className="header-item">Contact</div>
+                  </ButtonBase>
+              </div>
+            </div>
+          </div>
+          <div className="content">
+            <Route exact path="/" component={Home} />
+          </div>
+        </div>
+      </ThemeProvider>
+    </HashRouter>
   );
 }
 
 export default App;
+
+const theme = createMuiTheme({
+  palette: {
+      primary: {
+          light: "#ffcdcd",
+          main: "#eba5a5",
+          dark: "#c07c7c",
+          contrastText: "#ffcdcd"
+      },
+      secondary: {
+        light: "#a9dcc3",
+        main: "#85bca1",
+        dark: "#52866d",
+        contrastText: "#c8f6e0"
+    },
+  }
+})
